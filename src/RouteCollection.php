f6200fe51be4da73a0194b000261b2c407c589fe
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-08-28 18:22:51 -0300

    Feature :: Constructing RouteCollection File Structure

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -3,5 +3,52 @@
 namespace Src;
 
 class RouteCollection {
-    // to do
+    protected $routes_post   = [];
+    protected $routes_get    = [];
+    protected $routes_put    = [];
+    protected $routes_delete = [];
+
+    public function add($request_type, $pattern, $callback) {
+        switch ($request_type) {
+            case "post":
+                return $this->addPost($pattern, $callback);
+            break;
+            case "get":
+                return $this->addGet($pattern, $callback);
+            break;
+            case "put":
+                return $this->addPut($pattern, $callback);
+            break;
+            case "delete":
+                return $this->addDelete($pattern, $callback);
+            break;
+            default:
+                throw new \Exception("Invalid HTTP METHOD");
+        }
+    }
+
+    public function where($request_type, $pattern) {
+        // to do
+    }
+
+    public function definePattern($pattern) {
+        $pattern = implode('/', array_filter(explode('/', $pattern)));
+        return '/^' . str_replace('/', '\/', $pattern);
+    }
+
+    protected function addPost($pattern, $callback) {
+        // to do
+    }
+
+    protected function addGet($pattern, $callback) {
+        // to do
+    }
+
+    protected function addPut($pattern, $callback) {
+        // to do
+    }
+
+    protected function addDelete($pattern, $callback) {
+        // to do
+    }
 }
