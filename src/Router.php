d0830d5ce16dd6e9067484acf6f7de1eaf29080c
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-09-01 21:02:39 -0300

    Feature :: Constructing Router File Structure

--- src/Router.php
+++ src/Router.php
@@ -2,9 +2,45 @@
 
 namespace Src;
 
+use Src\Request; 
 use Src\Dispatcher;
 use Src\RouteCollection;
 
 class Router {
-    // to do
+    protected $route_collection;
+
+    public function __construct()
+    {
+        $this->route_collection = new RouteCollection;
+        $this->dispatcher       = new Dispatcher;
+    }
+
+    public function get($pattern, $callback)
+    {
+        $this->route_collection->add("get", $pattern, $callback);
+        return $this;
+    }
+
+    public function post($pattern, $callback)
+    {
+        $this->route_collection->add('post', $pattern, $callback);
+        return $this;   
+    }
+ 
+    public function put($pattern, $callback)
+    {  
+        $this->route_collection->add('put', $pattern, $callback);
+        return $this;   
+    }
+ 
+    public function delete($pattern, $callback)
+    {
+        $this->route_collection->add('delete', $pattern, $callback);
+        return $this;   
+    }
+
+    public function find($request_type, $pattern)
+    {
+        return $this->route_collection->where($request_type, $pattern);
+    }
 }
